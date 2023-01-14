56f8684c03d2c2a8be3b15b564f41eabca0415c1
Dz <MathBeWithCode@gmail.com>
2023-01-15 00:44:09 +0800

    sleep

---
+++ example/Learn_You_a_Haskell_for_Great_Good/Making_Our_Own_Types_and_Typeclasses/queryAvailableLocker/main.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
+import Data.Map qualified as Map
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+  case Map.lookup lockerNumber map of
+    Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"
+    Just (state, code) ->
+      if state /= Taken
+        then Right code
+        else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+lockers :: LockerMap
+lockers =
+  Map.fromList
+    [ (100, (Taken, "ZD39I")),
+      (101, (Free, "JAH3I")),
+      (103, (Free, "IQSA9")),
+      (105, (Free, "QOTSA")),
+      (109, (Taken, "893JJ")),
+      (110, (Taken, "99292"))
+    ]
+
+main = do
+  print $ lockerLookup 101 lockers
+  print $ lockerLookup 100 lockers
+  print $ lockerLookup 102 lockers
+  print $ lockerLookup 110 lockers
+  print $ lockerLookup 105 lockers
